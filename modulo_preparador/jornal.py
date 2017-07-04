14ea60d179888db9ebe789bf3890ba75e085e372
Janderson de Almeida <jandersoncoelho@gmail.com>
2017-07-03 22:08:10 -0300

    Alteração do arquivo .gitignore e criação de método para executar o OCR usando o shell do linux. Foi criado o Método gera_ocr na classe Jornal para realizar esse procedimento

--- modulo_preparador/jornal.py
+++ modulo_preparador/jornal.py
@@ -3,93 +3,98 @@ from datetime import datetime
 from PyPDF2 import PdfFileMerger, PdfFileReader, PdfFileWriter
 import time
 import img2pdf
+import os
+
 
 class Jornal(Documento):
 
-	def __init__(self,dados_jornal=dict):
+    def __init__(self, dados_jornal=dict):
 
-		self._data_publicacao 		= dados_jornal['data_publicacao']
-		self._titulo_jornal       	= dados_jornal['titulo_jornal']
-		self._descricao_jornal      = dados_jornal['descricao_jornal']
-		self._cidade	   			= dados_jornal['cidade']
-		self._estado				= dados_jornal['estado']
-		self._nome_arquivo 			= self._formata_nome_arquivo()
-		self._lista_imagens         = dados_jornal['imagens']
-			
-	def _formata_nome_arquivo(self):
+        self._data_publicacao = dados_jornal['data_publicacao']
+        self._titulo_jornal = dados_jornal['titulo_jornal']
+        self._descricao_jornal = dados_jornal['descricao_jornal']
+        self._cidade = dados_jornal['cidade']
+        self._estado = dados_jornal['estado']
+        self._nome_arquivo = self._formata_nome_arquivo()
+        self._lista_imagens = dados_jornal['imagens']
 
-		try:
-			#invertendo a data digitada pelo usuário para o formato 'ano_mes_dia'
-			data_string = self._data_publicacao.replace('/', '_',3)
-			data_convertida =  datetime.strptime(data_string, '%d_%m_%Y')	
-			data_reformatada_string = data_convertida.strftime('%Y_%m_%d')
-		
-			#transfomando o título do jornal para o nome do arquivo
-			titulo_jornal = self._titulo_jornal.replace(' ','_')
-			titulo_jornal = titulo_jornal.lower()
-			nome_arquivo_formatado = data_reformatada_string + '_' + titulo_jornal + '.pdf'
-			return nome_arquivo_formatado
-		
-		except ValueError as e:
-			raise e
-		
+    def _formata_nome_arquivo(self):
 
-	def _data_publicacao_valida(self):
-		try:
-			data_valida = time.strptime(self._data_publicacao, '%m/%d/%Y')
-			return True
-		except ValueError:
-			return False
+        try:
+            # invertendo a data digitada pelo usuário para o formato 'ano_mes_dia'
+            data_string = self._data_publicacao.replace('/', '_', 3)
+            data_convertida = datetime.strptime(data_string, '%d_%m_%Y')
+            data_reformatada_string = data_convertida.strftime('%Y_%m_%d')
 
+            # transfomando o título do jornal para o nome do arquivo
+            titulo_jornal = self._titulo_jornal.replace(' ', '_')
+            titulo_jornal = titulo_jornal.lower()
+            nome_arquivo_formatado = data_reformatada_string + '_' + titulo_jornal + '.pdf'
+            return nome_arquivo_formatado
 
-	@property
-	def dia(self):
-		return self._dia 
+        except ValueError as e:
+            raise e
 
-	@property
-	def mes(self):
-		return self._mes
+    def _data_publicacao_valida(self):
+        try:
+            data_valida = time.strptime(self._data_publicacao, '%m/%d/%Y')
+            return True
+        except ValueError:
+            return False
 
-	@property
-	def ano(self):
-		return self._ano
+    @property
+    def dia(self):
+        return self._dia
 
-	@property
-	def cidade(self):
-		return self._cidade
+    @property
+    def mes(self):
+        return self._mes
 
-	@property
-	def estado(self):
-		
-		return self._estado
+    @property
+    def ano(self):
+        return self._ano
 
-	@property
-	def descricao_jornal(self):		
-		return self._descricao_jornal
+    @property
+    def cidade(self):
+        return self._cidade
 
-	@property
-	def nome_arquivo(self):
-		return self._nome_arquivo 
+    @property
+    def estado(self):
 
-	@property
-	def titulo_jornal(self):
-		return self._titulo_jornal
+        return self._estado
 
-	#data de publicação do jornal
-	@property
-	def data_publicacao(self):
-		#self._data_publicacao = ('%s/%s/%s' % (self._dia, self._mes, self._ano))
-		if (self._data_publicacao_valida() == True):
-			return self._data_publicacao
-		else:
-			raise ValueError('A data de publicação do jornal está inválida!')
+    @property
+    def descricao_jornal(self):
+        return self._descricao_jornal
 
+    @property
+    def nome_arquivo(self):
+        return self._nome_arquivo
 
-	def gera_arquivo_jornal(self):
+    @property
+    def titulo_jornal(self):
+        return self._titulo_jornal
 
-		with open(self._nome_arquivo,"wb") as f:
-			self._lista_imagens.sort()
-			f.write(img2pdf.convert(self._lista_imagens))
-			f.close()
+    # data de publicação do jornal
+    @property
+    def data_publicacao(self):
+        #self._data_publicacao = ('%s/%s/%s' % (self._dia, self._mes, self._ano))
+        if (self._data_publicacao_valida() == True):
+            return self._data_publicacao
+        else:
+            raise ValueError('A data de publicação do jornal está inválida!')
 
-		
+    def gera_arquivo_jornal(self):
+
+        with open(self._nome_arquivo, "wb") as f:
+            self._lista_imagens.sort()
+            f.write(img2pdf.convert(self._lista_imagens))
+            f.close()
+
+    def gera_ocr(self, nome_arquivo=str):
+        try:
+            comando_ocr = "ocrmypdf -d -r -v --jobs 1 --title '%s' -l por %s %s" % (
+                self._titulo_jornal, self.nome_arquivo, self.nome_arquivo)
+        except Exception as e:
+            raise e
+        return os.system(comando_ocr)
--- modulo_preparador/test_proc_jornal.py
+++ modulo_preparador/test_proc_jornal.py
@@ -4,84 +4,92 @@ import glob
 from jornal import Jornal
 import os
 
+
 class TestProcessaJornal(unittest.TestCase):
-	
-	def test_cria_objeto_jornal(self):
-		dados_jornal = { 
-		                 'titulo_jornal':'Folha de Ituiutaba',
-						 'data_publicacao':'10/10/1959',
-						 'cidade':'Ituiutaba',
-						 'estado':'MG',
-						 'descricao_jornal':'',
-						 'imagens':[]	
-					   }
-		jornal = Jornal(dados_jornal)
-		assert isinstance(jornal, Jornal)
-		del(jornal)
 
-	def test_valida_dados_jornal(self):
+    def test_cria_objeto_jornal(self):
+        dados_jornal = {
+            'titulo_jornal': 'Folha de Ituiutaba',
+            'data_publicacao': '10/10/1959',
+            'cidade': 'Ituiutaba',
+            'estado': 'MG',
+            'descricao_jornal': '',
+            'imagens': []
+        }
+        jornal = Jornal(dados_jornal)
+        assert isinstance(jornal, Jornal)
+        del(jornal)
 
-		#testando se o usuário colocou a data certa
-		dados_jornal = { 
-		                 'titulo_jornal':'Folha de Ituiutaba',
-						 'data_publicacao':'10/10/1959',
-						 'cidade':'Ituiutaba',
-						 'estado':'MG',
-						 'descricao_jornal':'O jornal é teste!',	
-						 'imagens':[]
-					   }
+    def test_valida_dados_jornal(self):
 
-		jornal = Jornal(dados_jornal)
+        # testando se o usuário colocou a data certa
+        dados_jornal = {
+            'titulo_jornal': 'Folha de Ituiutaba',
+            'data_publicacao': '10/10/1959',
+            'cidade': 'Ituiutaba',
+            'estado': 'MG',
+            'descricao_jornal': 'O jornal é teste!',
+            'imagens': []
+        }
 
-		self.assertEqual(jornal.titulo_jornal, 'Folha de Ituiutaba')
-		self.assertEqual(jornal.data_publicacao, '10/10/1959')
-		self.assertEqual(jornal.cidade, 'Ituiutaba')
-		self.assertEqual(jornal.estado, 'MG')
-		self.assertEqual(jornal.descricao_jornal, 'O jornal é teste!')
-		self.assertEqual(jornal.nome_arquivo, '1959_10_10_folha_de_ituiutaba.pdf')
+        jornal = Jornal(dados_jornal)
 
-		del(jornal)
-		
-		
-	def test_valida_data_publicacao_jornal(self):
-		#testando se o usuário colocou a data errada
-		dados_jornal = { 
-		                 'titulo_jornal':'Folha de Ituiutaba',
-						 'data_publicacao':'31/02/1959',
-						 'cidade':'Ituiutaba',
-						 'estado':'MG',
-						 'descricao_jornal':''	,
-						 'imagens':[]
+        self.assertEqual(jornal.titulo_jornal, 'Folha de Ituiutaba')
+        self.assertEqual(jornal.data_publicacao, '10/10/1959')
+        self.assertEqual(jornal.cidade, 'Ituiutaba')
+        self.assertEqual(jornal.estado, 'MG')
+        self.assertEqual(jornal.descricao_jornal, 'O jornal é teste!')
+        self.assertEqual(jornal.nome_arquivo,
+                         '1959_10_10_folha_de_ituiutaba.pdf')
 
-					   }
-				
-		with self.assertRaises(ValueError):
-			jornal = Jornal(dados_jornal)
-			print('valor de data incorrero')
-			del(jornal)
+        del(jornal)
 
-	def test_gera_arquivo_pdf_jornal_sem_ocr(self):
-		lista_imagens = glob.glob('/home/janderson/pesquisa_pibit/jornais/pendrive_cepdomp/1960_06_correio_do_pontal/*.JPG')
-		dados_jornal = {
-						'titulo_jornal':'Correio do Pontal',
-	 					'data_publicacao':'10/06/1960',
-	 					'cidade':'Ituiutaba',
-	 					'estado':'MG',
-	 					'descricao_jornal':'O jornal é teste!',
-	 					'imagens':lista_imagens	
-	 				   }
-		jornal = Jornal(dados_jornal)
-		jornal.gera_arquivo_jornal()
-		self.assertTrue(os.path.getsize(jornal.nome_arquivo) > 0.0)
-		
-		
-        
+    def test_valida_data_publicacao_jornal(self):
+        # testando se o usuário colocou a data errada
+        dados_jornal = {
+            'titulo_jornal': 'Folha de Ituiutaba',
+            'data_publicacao': '31/02/1959',
+            'cidade': 'Ituiutaba',
+            'estado': 'MG',
+            'descricao_jornal': ''	,
+            'imagens': []
 
+        }
+
+        with self.assertRaises(ValueError):
+            jornal = Jornal(dados_jornal)
+            print('valor de data incorrero')
+            del(jornal)
+
+    def test_gera_arquivo_pdf_jornal_sem_ocr(self):
+        lista_imagens = glob.glob(
+            '/home/janderson/pesquisa_pibit/jornais/pendrive_cepdomp/1960_06_correio_do_pontal/*.JPG')
+        dados_jornal = {
+            'titulo_jornal': 'Correio do Pontal',
+            'data_publicacao': '10/06/1960',
+            'cidade': 'Ituiutaba',
+            'estado': 'MG',
+            'descricao_jornal': 'O jornal é teste!',
+            'imagens': lista_imagens
+        }
+        jornal = Jornal(dados_jornal)
+        jornal.gera_arquivo_jornal()
+        self.assertTrue(os.path.getsize(jornal.nome_arquivo) > 0.0)
+
+    def test_gera_ocr(self):
+        lista_imagens = glob.glob('/home/janderson/pesquisa_pibit/jornais/pendrive_cepdomp/1960_06_correio_do_pontal/*.JPG')
+        dados_jornal = {
+            'titulo_jornal': 'Correio do Pontal',
+            'data_publicacao': '10/06/1960',
+            'cidade': 'Ituiutaba',
+            'estado': 'MG',
+            'descricao_jornal': 'O jornal é teste!',
+            'imagens': lista_imagens
+        }
+        jornal = Jornal(dados_jornal)
+        self.assertEqual(jornal.gera_ocr(jornal.nome_arquivo), 0)
 if __name__ == '__main__':
- 	unittest.main()
-	
+    unittest.main()
 
-
-	#hangout com Kent Back sobre TDD
-	#https://www.youtube.com/watch?v=z9quxZsLcfo
-		
+    # hangout com Kent Back sobre TDD
+    # https://www.youtube.com/watch?v=z9quxZsLcfo
